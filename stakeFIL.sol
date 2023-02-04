d04bd85880fa22dddfec6ca2482b9429cecac86d
sarvagna <sarvagnakadiya1@gmail.com>
2023-02-05 00:28:44 +0530

    changes in stakeFIL contract

--- stakeFIL.sol
+++ stakeFIL.sol
@@ -1,30 +1,46 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity >=0.8.0 <=0.8.17;
 contract stakeFIL{
-    mapping(address => uint256) userStakeMapping;
-    mapping(address => uint256) nowEpoch;
-    mapping(address => uint256) withdrawEligible;
-    function stake(uint256 _duration) public payable {
-        userStakeMapping[msg.sender] += msg.value;
-        nowEpoch[msg.sender] = block.timestamp;
-        withdrawEligible[msg.sender] = block.timestamp + _duration;
+    address owner;
+    constructor() {
+        //setting up owner as deployer(paradigm)
+      owner = msg.sender;
     }
-    function readStake() public view returns(uint256) {
+    uint256 duration;
+
+    mapping(address => uint256) userStakeMapping;
+    mapping(address => uint256) stakeTimeEpochMapping;
+    mapping(address => uint256) withdrawEligibleMapping;
+
+    modifier onlyOwner {
+      require(msg.sender == owner);
+      _;
+    }
+    function setDuration(uint256 _durationInSeconds) public onlyOwner{
+        duration = _durationInSeconds;
+    }
+    function stake(uint256 _duration) public payable {
+        // 15552000 for 6 months
+        require(_duration >= duration,"Minimum duration doesn't match");
+        userStakeMapping[msg.sender] += msg.value;
+        stakeTimeEpochMapping[msg.sender] = block.timestamp;
+        withdrawEligibleMapping[msg.sender] = block.timestamp + _duration;
+    }
+    function readUserStake() public view returns(uint256) {
         return userStakeMapping[msg.sender];
     }
-    function readEpoch() public view returns(uint256) {
-        return nowEpoch[msg.sender];
+    function readStakeTimeEpoch() public view returns(uint256) {
+        return stakeTimeEpochMapping[msg.sender];
     }
     function readWithdrawEligibility() public view returns(uint256) {
-        return withdrawEligible[msg.sender];
+        return withdrawEligibleMapping[msg.sender];
     }
     function rightNow() public view returns(uint256){
-
-    return block.timestamp;
+        return block.timestamp;
     }
    
     function withdraw(address payable _address, uint256 _value) public payable {
-        require(block.timestamp > withdrawEligible[msg.sender],"please wait");
+        require(block.timestamp > withdrawEligibleMapping[msg.sender],"please wait");
         _address.transfer(_value);
         userStakeMapping[msg.sender] = userStakeMapping[msg.sender] - _value;
     }
